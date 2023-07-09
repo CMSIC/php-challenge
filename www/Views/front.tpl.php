4f035096fdff86e27fb00f3000dd097af1100f3e
adibhamchi <hamchiadib@gmail.com>
2023-07-09 19:41:04 +0200

    Sample homepage + add film modal

--- www/Views/front.tpl.php
+++ www/Views/front.tpl.php
@@ -5,11 +5,13 @@
     <title>Front</title>
     <meta name="description" content="ceci est un super site">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" />
 </head>
 <body>
     <main class="w-screen h-screen bg-gray-800">
         <!-- inclure la vue -->
         <?php include $this->view;?>
     </main>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
 </body>
 </html>
